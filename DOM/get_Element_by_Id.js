f6f106d5f15096a7bade8aac020546ccb0f75d02
HmUmaxKhan <mukhan.bese22seecs@seecs.edu.pk>
2023-07-27 22:40:27 +0500

    DOM: Use of  getElementById

---
+++ DOM/get_Element_by_Id.js
@@ -0,0 +1,7 @@
+// Get element by id is used to select any element having specific id as an object.
+
+const mainHeading = document.getElementById("main-heading");
+
+console.log(mainHeading);
+console.log(typeof (mainHeading));
+console.dir(mainHeading);
