5a2376e6e334693c436143f49e202a4dbce2d951
HmUmaxKhan <mukhan.bese22seecs@seecs.edu.pk>
2023-07-27 09:45:44 +0500

    OOP: Inheritance and Use of Super keyword

---
+++ OOP/inheritance_super_keyword.js
@@ -0,0 +1,43 @@
+// Inheritance is the most imp in OOP.
+
+// We can use inheritance using extends key word in JS
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    info() {
+        console.log(`${this.name} is ${this.age} years old`);
+    }
+}
+
+    class Teacher extends Person {
+
+    }
+
+    const Ali = new Teacher("Ali",27);
+
+    console.log(Ali);
+    Ali.info();
+
+
+    // Now we use super keyword.
+
+    class Student extends Person {
+        constructor(name, age, subject) {
+            super(name, age);
+            this.subject = subject;
+        }
+        info() {
+                    console.log(`${this.name} is ${this.age} years old and ${this.subject} is favourite`);
+                }
+        sub() {
+            console.log(`${this.name} is ${this.age} years old and ${this.subject}`);
+        }
+    }
+
+    const Ali1 = new Student("Ali",27,"Math");
+    console.log(Ali1);
+    Ali1.info();
+    Ali1.sub();
