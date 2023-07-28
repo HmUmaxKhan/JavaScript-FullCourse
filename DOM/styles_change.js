6edf83ee1e6c86e667bfb54a997cbce9cfa145f3
HmUmaxKhan <mukhan.bese22seecs@seecs.edu.pk>
2023-07-28 21:49:58 +0500

    DOM: Chnage Styles using JavaScript

---
+++ DOM/styles_change.js
@@ -0,0 +1,7 @@
+// In this we will change styles using JavaScript
+
+const changeStyle = document.querySelector("#main-heading");
+
+changeStyle.style.color = "gray";
+
+changeStyle.style.backgroundColor = "white"
