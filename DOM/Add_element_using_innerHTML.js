e27171fac35e21f163538452a94830af807222ff
HmUmaxKhan <mukhan.bese22seecs@seecs.edu.pk>
2023-07-29 13:50:56 +0500

    DOM: Using innerHTML to change the existing Html elements

---
+++ DOM/Add_element_using_innerHTML.js
@@ -0,0 +1,13 @@
+// Adding new html elements using inner html
+
+const ul = document.querySelector(".todo-list li");
+console.log(ul);
+
+// Changing inner html
+// ul.innerHTML = "<li>ALi</li>"
+
+// Adding new html elements using inner html
+
+// ul.innerHTML += "ALi"
+
+// But it is not suitable option , we used it to only change the Element
