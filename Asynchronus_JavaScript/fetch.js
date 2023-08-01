261341ae7ec68e82f19189857820464427f12b5e
HmUmaxKhan <mukhan.bese22seecs@seecs.edu.pk>
2023-08-01 23:18:55 +0500

    Asynchronus JavaScript: Fetch Request.

---
+++ Asynchronus_JavaScript/fetch.js
@@ -0,0 +1,27 @@
+// In this we can use the fetch instead of xml request
+
+
+// For example
+
+const URL = "https://jsonplaceholder.typicode.com/posts";
+
+fetch(URL)
+.then((res)=>{
+   return res.json();
+}).then((res)=>{
+    console.log(res);
+   const id = res[5].id;
+   console.log(id);
+   return id;
+}).then((id)=>{
+   const url = `${URL}/${id}`;
+   return fetch(url);
+}).then((req)=>{
+    return req.json();
+}).then((res)=>{
+    console.log(res);
+})
+
+
+// In this we have to return resolve.json() to another promise in order to console.log() it.
+
