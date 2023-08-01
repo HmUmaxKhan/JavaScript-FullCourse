80a529fc050c7281ba03e0607ea28570d423a6d5
HmUmaxKhan <mukhan.bese22seecs@seecs.edu.pk>
2023-08-01 10:34:55 +0500

    Asynchronus JavaScript: AJAX Basic theory.

---
+++ Asynchronus_JavaScript/AJAX_Basic_theory.js
@@ -0,0 +1,22 @@
+// BASIC THEORY
+
+// AJAX : Asynchronus JavaScript and XML
+
+// is a set of "web development techniques"
+// using many web technologies on the "client side"
+// to create a asynchronus web applications.
+
+
+// With AJAX, web applications can send and receive
+// data from the server asynchronusly
+// without intefering with the display
+
+
+// We don't use data in XML form anymore
+// We use JSON now
+
+// 3 ways to create and send requests to the server
+
+// 1.xmlHTTPRequest(old method)
+// 2. fetch api (new way)
+// 3. axios (third party library)
